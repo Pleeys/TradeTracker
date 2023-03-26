877cf7c36858a9681283f6766941446e289d5fec
Tomasz Kocik <tomasz.kocik00@gmail.com>
2023-03-26 03:01:43 +0200

    Create and update posts

--- tradetracker/forms.py
+++ tradetracker/forms.py
@@ -61,5 +61,5 @@ class UpdateAccountForm(FlaskForm):
         
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content',validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
