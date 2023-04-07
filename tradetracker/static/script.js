b108465acf002d833a19d478f70e89606f6be9f3
Tomasz Kocik <tomasz.kocik00@gmail.com>
2023-04-07 03:14:49 +0200

    Responsive css

--- tradetracker/static/script.js
+++ tradetracker/static/script.js
@@ -1,11 +1,12 @@
 let btn = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
 let trade_tracker_logo = document.querySelector('.trade_tracker_logo');
+let navbar_right = document.querySelector('.navbar-right');
 
 btn.onclick = function () {
     sidebar.classList.toggle('active');
     trade_tracker_logo.classList.toggle('active');
-    
+    navbar_right.classList.toggle('active');
 };
 
 var cPass = document.getElementById("c-pass");
